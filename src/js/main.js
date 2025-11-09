4edd39fcad46a6dab73109a3560d7d1fbae87904
Andrii Ferenchuk <ferenchuk.a@gmail.com>
2025-11-09 17:38:00 +0200

    refactor: big styles refactor

--- src/js/main.js
+++ src/js/main.js
@@ -7,8 +7,18 @@ window.onload = async () => {
     footer()
     burger()
     await getProductsArr()
-    renderProductsByRange(".products-selected", "selected-product-card", 3, 7)
-    renderProductsByRange(".new-products", "selected-product-card", 7, 11)
+    renderProductsByRange(
+      ".products-grid-wrapper.selected-products-wrapper",
+      "selected-product-card",
+      3,
+      7
+    )
+    renderProductsByRange(
+      ".products-grid-wrapper.new-products-wrapper",
+      "selected-product-card",
+      7,
+      11
+    )
     renderProductsByRange(".carousel-track", "product-card")
     renderProductsByRange(".catalog_container", "selected-product-card", 4, 18)
     renderProductsByRange(".top-best-sets", "top-card_container", 19, 25)
